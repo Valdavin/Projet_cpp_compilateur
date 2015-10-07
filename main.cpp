77c0ab1d3a50674c1b097057f92e8d21370c7eb2
Valentin Davin <dj_valentin@hotmail.fr>
2015-10-07 13:53:31 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: davinv
+ *
+ * Created on 7 octobre 2015, 13:50
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
